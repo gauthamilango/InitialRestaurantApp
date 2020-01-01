2aabde81f522c19fc53287ecbf76f66b7e68e327
Gautham Ilango <gauthamilango@gmail.com>
2020-01-01 20:38:30 +0530

    Update to the Model Dish

--- RestaurantMenu/RestaurantMenu/Model/Dish.swift
+++ RestaurantMenu/RestaurantMenu/Model/Dish.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Dish: Codable, Hashable, Identifiable {
   var id: Int
@@ -14,4 +15,10 @@ struct Dish: Codable, Hashable, Identifiable {
   var cuisine: String
   var price: Int
   var imageName: String
+}
+
+extension Dish {
+    var image: Image {
+        RestaurantImageStore.shared.image(name: imageName)
+    }
 }
